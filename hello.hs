f4ff26133a9e517f7bcb18bdc0fc6fd036ac2ac4
Nerdeee <48033320+Nerdeee@users.noreply.github.com>
2023-06-22 13:17:24 -0700

    Update hello.hs

--- hello.hs
+++ hello.hs
@@ -1,3 +1,4 @@
+ main = do
   print "my first haskell program"
   name <- getLine
   print ("Hello, " ++ name)
@@ -101,4 +102,31 @@ describeList :: [a] -> String
 describeList xs = "The list is " ++ case xs of [] -> "Empty"
                                                [x] -> "Singleton list"
                                                [xs] -> "A longer list"
-  -- end of syntax in functions chapter (should've added this comment for the previous chapter but I'm lazy)
+-- end of syntax in functions chapter (should've added this comment for the previous chapter but I'm lazy)
+maximum' :: (Ord a) => [a] -> a
+maximum' [] = error "empty list, no max value"
+maximum' [x] = x
+maximum' (x:xs)
+  | x > maxTail = x
+  | otherwise = maxTail
+  where maxTail maximum' xs
+
+take' :: (Num i, Ord i) => i -> [a] -> [a]
+take' n _
+  | n <= 0 = []
+take' _ [] = []
+take' n (x:xs) = x:take' (n-1) xs
+
+zip' :: [a] -> [b] -> [(a,b)]
+zip' [] _ = []
+zip' _ [] = []
+zip' (x:xs) (y:ys) = (x:y):zip' xs ys
+
+quicksort :: (Ord a) => [a] -> [a]  
+quicksort [] = []  
+quicksort (x:xs) =   
+    let smallerSorted = quicksort [a | a <- xs, a <= x]  
+        biggerSorted = quicksort [a | a <- xs, a > x]  
+    in  smallerSorted ++ [x] ++ biggerSorted  
+
+-- end of recursion chapter
