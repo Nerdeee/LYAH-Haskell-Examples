4fce7ed7aff19cbcf137189884c3d476c7294801
Nerdeee <48033320+Nerdeee@users.noreply.github.com>
2023-06-26 17:57:15 -0700

    Update hello.hs

--- hello.hs
+++ hello.hs
@@ -142,3 +142,25 @@ isUpperAlphanum = (`elem` ['A'..'Z'])
 flip' :: (a -> b -> c) -> (b -> a -> c)
 flip' f = g
   where g x y = f y x
+
+ largestDivisible :: (Integral a) => a
+largestDivisible = head (filter p [100000, 99999..])
+  where p x = x `mod` 3829 == 0 
+
+chain :: (Integral a) => a -> [a]
+chain 1 = [1]
+chain n
+  | even n = n:chain (n `div` 2)
+  | odd n = n:chain (n*3+1)
+
+numLongChains :: Int
+numLongChains = length (filter isLong (map chain [1..100]))
+  where isLong xs = length xs > 15
+
+sumByFolding :: (Num a) => [a] -> a
+sumByFolding xs = foldl(\a b -> a + b) 0 xs
+
+--rightFoldMap :: (a -> b) -> [a] -> [b]
+--rightFoldMap f xs = (\x acc -> f x: acc ) [] xs
+
+
